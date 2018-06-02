ebe14ad274a2902410f930a8f7b7b08f68ca1806
jvmatamu <39078700+jvmatamu@users.noreply.github.com>
2018-06-02 11:18:42 +0700

    config network stats

--- website_example/config.js
+++ website_example/config.js
@@ -11,3 +11,19 @@ var transactionExplorer = "http://chainradar.com/{symbol}/transaction/{id}";
 
 var themeCss = "themes/default.css";
 var defaultLang = 'en';
+
+var networkStat = {
+    "ird": [
+        ["irdpool.fr", "https://irdpool.fr:8117"],
+        ["irdpool.eu", "https://irdpool.eu:8443"],
+        ["irdpool.ca", "https://irdpool.ca:8117"],
+        ["irdpool.net", "https://irdpool.net:8117"],
+        ["cryptoknight.cc/iridium", "https://cryptoknight.cc/rpc/iridium"],
+        ["irdpool.ru", "https://mig.ird.cash/ru"],
+        ["iridiumpool.duckdns.org", "https://mig.ird.cash/nl"],
+        ["irdpool.online", "https://mig.ird.cash/onl"],
+        ["ird.cryptoisme.com", "https://mig.ird.cash/cryptoisme"],
+        ["coinpoolit.webhop.me", "https://mig.ird.cash/coinpoolit"],
+        ["matamu.me/iridium", "https://matamu.me:5119"]
+    ]
+};
